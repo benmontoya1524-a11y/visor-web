d13466520f8159680c995e53d80ffaefc1dbfca3
Forest <forestlaw5566@gmail.com>
2026-02-13 09:58:53 -0300

    Auto create superuser for production

--- viewer/apps.py
+++ viewer/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
 
-
 class ViewerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'viewer'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                password="admin12345"
+            )
